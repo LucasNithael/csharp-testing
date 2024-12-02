35cddd1659413af9c3fb444c942b95afc995d50e
Lucas Nithael <lucasnithael0101@gmail.com>
2024-12-01 23:52:41 -0300

    ajustes na solution

--- Demo.Tests/CalculadoreTests.cs
+++ Demo.Tests/CalculadoreTests.cs
@@ -1,16 +1,17 @@
-using Xunit;
-using Calculadora;
+﻿using Xunit;
 
 namespace Demo.Tests
 {
-    public class CalculadoreTests{
-        
+    public class CalculadoreTests
+    {
+
         [Fact]
-        public void Calculadora_Somar_RetornarValorSoma(){
+        public void Calculadora_Somar_RetornarValorSoma()
+        {
             // Arrange
             var calculadora = new Calculadora();
             // Act
-            var resultado = calculadora.Somar(2,2);
+            var resultado = calculadora.Somar(2, 2);
             //Assert
             Assert.Equal(4, resultado);
         }
