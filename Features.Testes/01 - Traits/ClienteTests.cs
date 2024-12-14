8666d11419ad3b9fdd26551b803b89c9baf5ff6b
Lucas Nithael <lucasnithael0101@gmail.com>
2024-12-14 18:18:39 -0300

    exemplos de testes unitários com o uso dos Traits, Fixtures e Asserts

---
+++ Features.Testes/01 - Traits/ClienteTests.cs
@@ -0,0 +1,56 @@
+﻿using Xunit;
+using Features.Clientes;
+
+namespace Features.Testes._01___Traits
+{
+    public class ClienteTests
+    {
+        [Fact(DisplayName = "Novo Cliente Válido")]
+        [Trait("Categoria", "Cliente Trait Testes")]
+        public void Cliente_NovoCliente_DeveEstarValido()
+        {
+            // Arrange 
+            var cliente = new Cliente(
+                Guid.NewGuid(),
+                "Fulano",
+                "Ciclano",
+                DateTime.Now.AddYears(-30),
+                "fulado@email.com",
+                true,
+                DateTime.Now
+                );
+
+            // Act
+            var result = cliente.EhValido();
+
+            // Assert
+            Assert.True(result);
+            Assert.Empty(cliente.ValidationResult.Errors);
+
+        }
+
+        [Fact(DisplayName = "Novo Cliente Inválido")]
+        [Trait("Categoria", "Cliente Trait Testes")]
+        public void Cliente_NovoCliente_DeveEstarInvalido()
+        {
+
+            // Arrange 
+            var cliente = new Cliente(
+                Guid.NewGuid(),
+                "",
+                "",
+                DateTime.Now.AddYears(-30),
+                "fulado@email.com",
+                true,
+                DateTime.Now
+                );
+
+            // Act
+            var result = cliente.EhValido();
+
+            // Assert
+            Assert.False(result);
+            Assert.NotEmpty(cliente.ValidationResult.Errors);
+        }
+    }
+}
---
+++ Features.Testes/01 - Traits/ClienteTestsFixture.cs
@@ -0,0 +1,47 @@
+﻿using Features.Clientes;
+using Xunit;
+
+namespace Features.Testes._01___Traits
+{
+    // Collection Permite que todos os testes de uma coleção compartilhem o mesmo contexto de uma instancia de uma classe
+    [CollectionDefinition(nameof(ClienteCollection))]
+    public class ClienteCollection : ICollectionFixture<ClienteTestsFixture>
+    {
+    }
+    public class ClienteTestsFixture : IDisposable
+    {
+        public Cliente GerarClienteValido()
+        {
+            var cliente = new Cliente(
+                Guid.NewGuid(),
+                "Fulano",
+                "Ciclano",
+                DateTime.Now.AddYears(-30),
+                "fulado@email.com",
+                true,
+                DateTime.Now
+                );
+
+            return cliente;
+        }
+
+        public Cliente GerarClienteInvalido()
+        {
+            var cliente = new Cliente(
+                Guid.NewGuid(),
+                "",
+                "",
+                DateTime.Now.AddYears(-30),
+                "fulado@email.com",
+                true,
+                DateTime.Now
+                );
+
+            return cliente;
+        }
+        public void Dispose()
+        {
+        }
+
+    }
+}
